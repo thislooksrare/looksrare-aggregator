42804e22f41ebed50d23db59ec15555f3a306309
LOOKS RARE <93809413+thislooksrare@users.noreply.github.com>
2022-03-23 12:24:54 +0100

    Update getListings.js

--- src/data/listings/getListings.js
+++ src/data/listings/getListings.js
@@ -1,4 +1,4 @@
-import apiClient from "../client/apiClient";
+/* import apiClient from "../client/apiClient";
 
 async function getListings() {
   try {
@@ -9,6 +9,42 @@ async function getListings() {
   } catch (error) {
     console.log(error);
   }
+}
+
+export default getListings;
+*/
+
+
+import axios from "axios";
+import getArtists from "../artists/getArtists";
+
+
+async function getListings() {
+  try {
+    let ArtistData = await getArtists(); 
+    let names = ArtistData.map((artist) => {
+      return artist.name;
+    });
+    let response = await getFromUrl(0, names);
+    var results = [];
+    for (const token of response.data.tokens) {
+      if (token.isListed) results.push(token);
+    }
+    return results;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getFromUrl(from, names) {
+  let url =
+    "https://api.exchange.art/v1/public/tokens?limit=10000&from=" +
+    from +
+    '&filters={"tokenStatus":["curated","certified","known"],"brands":["' +
+    names.join('","') +
+    '"]}';
+  let response = await axios.get(url);
+  return response;
 }
 
 export default getListings;
